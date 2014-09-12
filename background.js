90ad33dc2b99f90a4f10d0114a584a1e09040730
Bill Ross <billross@gmail.com>
2014-09-12 15:10:55 -0500

    Initial commit

---
+++ background.js
@@ -0,0 +1,5 @@
+(function($, tfsNotifier) {
+    $(document).ready(function() {
+        tfsNotifier.run(2000, 5000);
+    });    
+})(jQuery, tfsNotifier);
---
+++ tfsNotifier.js
@@ -0,0 +1,39 @@
+var tfsNotifier = (function($, window) {
+    var tfsNotifier = {};
+
+    function getLastMessage() {
+
+        var lastMessage = {
+            id: $('li.message.normal').last().attr('id'),
+            sender: $('li.message.normal').last().find('.message-sender').children('div:first-child').attr('title'),
+            image: $(location).attr('protocol') + '//' + $(location).attr('host') + $('.chat-box').find('.identity-picture').last().attr('src'),
+            message: $('li.message.normal').last().find('.message-text').text()
+        }
+
+        return lastMessage;
+    };
+
+    function run(interval, timeout) {
+        var lastMessage = getLastMessage();
+
+        window.setInterval(function() {
+            var currentMessage = getLastMessage();
+
+            if(lastMessage.id != currentMessage.id && lastMessage.id != undefined) {
+                var n = new Notification(currentMessage.sender, { body: currentMessage.message,
+                    icon: currentMessage.image });
+                n.onshow = function () {
+                    setTimeout(n.close, timeout);
+                }
+            }
+
+            lastMessage = currentMessage;
+
+        }, interval);
+    };
+
+    tfsNotifier.run = run;
+
+    return tfsNotifier;
+
+}(jQuery, window));
